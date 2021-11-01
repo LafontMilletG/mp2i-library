7418c185d0c21887dee2a6366e49144e92eb4cca
LafontMilletG <90219615+LafontMilletG@users.noreply.github.com>
2021-11-01 14:24:12 +0100

    Update sort.ml

--- algo/sort/sort.ml
+++ algo/sort/sort.ml
@@ -5,15 +5,18 @@ let swap t i j =
   let tmp = t.(i) in
   t.(i) <- t.(j);
   t.(j) <- tmp;;
-
+  
 (** [fusion_triée t1 t2] renvoie un tableau trié contenant les élements de t1 et t2, deux tableaux triés*)
-let fusion_triée t1 t2 = 
+let fusion_triee t1 t2 = 
 let k1 = ref 0 in
 let k2 = ref 0 in 
-let f = Array.make lenght t1 + lenght t2 0 in
-for i=0 to lenght f-1 do
-if k2 = lenght t2-1 then f.(i) <- t1.(k1)
-else f.(i)<-min t1.(k1) t2.(k2);;
+let f = Array.make ((Array.length t1) + (Array.length t2)) 0 in
+for i=0 to Array.length f-1 do
+if !k1 = Array.length t1 then (f.(i) <- t2.(!k2);k2 := !k2 +1)
+else if !k2 = Array.length t2 then (f.(i) <- t1.(!k1);k1 := !k1 +1)
+else if t1.(!k1)< t2.(!k2) then( f.(i) <- t1.(!k1);k1 := !k1 +1)
+else (f.(i) <- t2.(!k2); k2 := !k2 +1)
+done; f;;
 
 
 (** [sort_bubble t] sorts array t using bubble sorting (O(n**2)) *)
@@ -23,5 +26,11 @@ let sort_bubble t =
       if t.(j) > t.(j + 1) then swap t j (j + 1)
       done
   done;;
-
   
+(** [dicho t] renvoie le tableau t trié *)
+let dicho t = 
+    let rec inter t vb vh =
+    if vh-vb=1 then fusion_triee [|t.(vb)|] [|t.(vh)|]
+    else if vh-vb=0 then [|t.(vh)|]
+    else fusion_triee (inter t vb ((vb+vh)/2)) (inter t ((vh+vb)/2 +1) vh)
+    in inter t 0 (Array.length t-1);;
