3895df710a7d3d27ebb4c125e7b1b131481bd483
LafontMilletG <90219615+LafontMilletG@users.noreply.github.com>
2021-11-01 12:15:56 +0100

    Update sort.ml

--- algo/sort/sort.ml
+++ algo/sort/sort.ml
@@ -6,6 +6,16 @@ let swap t i j =
   t.(i) <- t.(j);
   t.(j) <- tmp;;
 
+(** [fusion_triée t1 t2] renvoie un tableau trié contenant les élements de t1 et t2, deux tableaux triés*)
+let fusion_triée t1 t2 = 
+let k1 = ref 0 in
+let k2 = ref 0 in 
+let f = Array.make lenght t1 + lenght t2 0 in
+for i=0 to lenght f-1 do
+if k2 = lenght t2-1 then f.(i) <- t1.(k1)
+else f.(i)<-min t1.(k1) t2.(k2);;
+
+
 (** [sort_bubble t] sorts array t using bubble sorting (O(n**2)) *)
 let sort_bubble t = 
   for _ = 0 to Array.length t - 1 do
@@ -13,3 +23,5 @@ let sort_bubble t =
       if t.(j) > t.(j + 1) then swap t j (j + 1)
       done
   done;;
+
+  
